561e10f35fa5681d806882c92ef44c106d506feb
Olalekan Efunkunle <efunkunleolalekan@gmail.com>
2020-11-02 15:07:53 +0100

    added color and ellipse

--- src/App.js
+++ src/App.js
@@ -8,19 +8,66 @@ function App() {
   
 
   const [ state, setState] = useState({
-    shape:null
+    polyg:1, 
+    polyl:1,
+    polygon:"0 ",
+        polyline:'0 ',
+    shape:null,
+    width:'',
+        height:'',
+        circle:'',
+        line:'',
+        xradius:'',
+        yradius:'',
+        x1:'',
+        x2:'',
+        y1:'',
+        y2:'',
+        color:'blue'
   })
-  const handleChange =(e)=>{
-     const shape = e.target.value
-     setState({shape:shape})
+  
+
+
+  const handlePoint =(vector)=>{
+        
+    setState({
+        ...state,
+        [vector]:state[vector]+1
+    })
+    console.log([vector])
+}
+
+const RemovePoint =(vector)=>{
+  if (state[vector]>1){
+      setState({
+          
+          ...state,
+          [vector]:state[vector]-1
+      
+      })
+     
   }
+  
+}
+
+const handleChange =(e)=>{
+  const {name, value} = e.target
+  setState({
+      ...state,
+      [name]:value
+  })
+   
+ }
+
+
   console.log(state.shape)
   return (
     <div className="App">
     <div>
+    <input type='color' name="color" onChange={handleChange}/>
     <label for="select-shape">Choose a pet:</label>
 
-    <select onChange={handleChange} name="pets" id="shape-select">
+    <select onChange={handleChange} name="shape" id="shape-select">
         <option value={null}>--Please choose an option--</option>
         <option value="Circle">Circle</option>
         <option value="Rectangle">Rectangle/Square</option>
@@ -31,7 +78,11 @@ function App() {
     </select>
     </div>
     
-    <Shape  shape={state.shape}/>
+    <Shape  
+    state={state}
+    handleChange={handleChange}
+    handlePoint = {handlePoint}
+    RemovePoint={RemovePoint}/>
 
     <Logo />
     
